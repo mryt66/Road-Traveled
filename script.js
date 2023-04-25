e1dc076a13e5d25c0f4691e0d4892c352e10c305
Marcin <64143856+mryt66@users.noreply.github.com>
2023-04-25 11:34:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,93 +1,48 @@
-const map = L.map('map'); 
-// Initializes map
-
-map.setView([51.505, -0.09], 13); 
-// Sets initial coordinates and zoom level
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+//Creating a map 
+    let map = L.map('map'); 
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 15,
     attribution: '© OpenStreetMap'
-}).addTo(map); 
-// Sets map data source and associates with map
+    }).addTo(map); 
+    var start = L.latLng(52.229676, 21.012229);
+    var end = L.latLng(52.406374, 16.925168);
+    var bounds = L.latLngBounds(start, end);
+    map.fitBounds(bounds);
 
-let marker, circle, zoomed;
+    var coords= [];
+    var last, distance;
+    var iterator=0;
+    var total_Distance=0;
+    var a=0;
+    var popup = L.popup();
 
-navigator.geolocation.watchPosition(success, error);
 
-function success(pos) {
+    function onMapClick(e) 
+    {
+        //Everytime you click somewhere, it creates a line and adds i
+        popup
+            .setLatLng(e.latlng)
+            let marker = L.marker(e.latlng).addTo(map);
+            coords.push([e.latlng.lat,e.latlng.lng]);
+            polyline = L.polyline(coords).addTo(map);
 
-    const lat = pos.coords.latitude;
-    const lng = pos.coords.longitude;
-    const accuracy = pos.coords.accuracy;
-
-    if (marker) {
-        map.removeLayer(marker);
-        map.removeLayer(circle);
-    }
-    // Removes any existing marker and circule (new ones about to be set)
-
-    marker = L.marker([lat, lng]).addTo(map);
-    circle = L.circle([lat, lng], { radius: accuracy }).addTo(map);
-    marker.bindPopup("You are here").openPopup();
-    // Adds marker to the map and a circle for accuracy
-
-    if (!zoomed) {
-        zoomed = map.fitBounds(circle.getBounds()); 
-    }
-    // Set zoom to boundaries of accuracy circle
-
-    map.setView([lat, lng]);
-    // Set map focus to current user position
-
-}
-
-function error(err) 
-{
-    if (err.code === 1) {
-        alert("Please allow geolocation access");
-    } else {
-        alert("Cannot get current location");
-    }
-}
-
-var states = [{
-    "type": "Feature",
-    "properties": {"party": "Republican"},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-104.05, 48.99],
-            [-97.22,  48.98],
-            [-96.58,  45.94],
-            [-104.03, 45.94],
-            [-104.05, 48.99]
-        ]]
-    }
-}, {
-    "type": "Feature",
-    "properties": {"party": "Democrat"},
-    "geometry": {
-        "type": "Polygon",
-        "coordinates": [[
-            [-109.05, 41.00],
-            [-102.06, 40.99],
-            [-102.03, 36.99],
-            [-109.04, 36.99],
-            [-109.05, 41.00]
-        ]]
-    }
-}];
-
-L.geoJSON(states, {
-    style: function(feature) {
-        switch (feature.properties.party) {
-            case 'Republican': return {color: "#ff0000"};
-            case 'Democrat':   return {color: "#0000ff"};
+        if (a>-1)
+        {
+            var pointA = L.latLng(coords[a]);
+            var pointB = L.latLng(coords[a+1]);
+            distance = pointA.distanceTo(pointB);
+            total_Distance+=parseInt(distance)/1000;
+            
         }
+        
+        document.getElementById('result').innerHTML = 'Entire distance equals: ' + total_Distance.toFixed(3) + ' km';
+        a++;
     }
-}).addTo(map);
 
 
+    function onunit(scaler, unit)
+    {
+        document.getElementById('result').innerHTML = 'Entire distance equals: ' + (total_Distance * scaler).toFixed(3) + ' '+unit;
+    }
 
-
-
+    map.on('click', onMapClick);
